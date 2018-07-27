08b4a7e920a09c8b55cac064cfb9cb3e5d0961fc
Raza <salmank22@yahoo.com>
2018-07-28 01:53:11 +0500

    Made the site just before responsive images comes into play

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,2 @@
-var gulp = require("gulp"),
-watch = require("gulp-watch");
-
-gulp.task("default", function(){
-	console.log("Horray I have installed gulp")
-});
-
-gulp.task("html", function(){
-	console.log("Hmtl task ran")
-});
-
-gulp.task("watch", function(){
-	watch("./app/index.html", function(){
-		gulp.start("html");
-	});
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
